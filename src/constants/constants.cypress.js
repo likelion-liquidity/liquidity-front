b1978febf700c2588eff6757ef27a160ece72dab
wwdbsh <wwdbsh@gmail.com>
2022-02-08 11:34:47 +0900

    20220208:setup

--- src/constants/constants.cypress.js
+++ src/constants/constants.cypress.js
@@ -1,3 +1,4 @@
 export const ACCESS_KEY_ID = process.env.REACT_APP_ACCESS_KEY_ID;
 export const SECRET_ACCESS_KEY = process.env.REACT_APP_SECRET_ACCESS_KEY;
+export const NFT_CONTRACT_ADDRESS = "0x00";
 export const CHAIN_ID = "8217";
