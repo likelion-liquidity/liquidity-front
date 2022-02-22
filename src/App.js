e7e18ffad1de51bc7cc92abe323ead4bd033e1a5
westkite1201 <westkite1201@gmail.com>
2022-02-22 15:52:22 +0900

    fix : import 오류 fix

--- src/App.js
+++ src/App.js
@@ -25,9 +25,6 @@ import LENDING_ABI from 'abi/LendingABI.json';
 import { getEosTokenAddress, getNftContract } from 'lib/api/UseTokenApi';
 import { getKlaytnProvider } from 'lib/helpers';
 
-import { getNftContract } from 'lib/api/UseTokenApi';
-
-
 const DATA_HOLDER_ADDRESS = '0x924965fFD912544AeeC612812F4aABD124278C1C';
 const LENDING_ADDRESS = '0xABa0111C2c6dd22A024608e302f9026958dB0688';
 // const KIP17_ADDRESS = '0xD11da04cC151CD54f046CE1F3Ea12afff2006757';
@@ -59,28 +56,36 @@ function App() {
   const getWhiteList = async () => {
     try {
       const klaytn = getKlaytnProvider(); // klaytn obj
-      const contract = caver.contract.create(DATAHOLDER_ABI, DATA_HOLDER_ADDRESS); // Dataholder contract
-      const lendingContract = caver.contract.create(LENDING_ABI, LENDING_ADDRESS); // Lendinng contract
-      
+      const contract = caver.contract.create(
+        DATAHOLDER_ABI,
+        DATA_HOLDER_ADDRESS
+      ); // Dataholder contract
+      const lendingContract = caver.contract.create(
+        LENDING_ABI,
+        LENDING_ADDRESS
+      ); // Lendinng contract
+
       /* 화이트 리스트 어드레스 받아오기  */
       const whiteListNFT = await contract.methods.getWhiteListNftList().call();
       const getNftDataContractCall = async (nftAddress) => {
         const nftData = await contract.methods.getNftData(nftAddress).call();
-        const { items : nfts } = (await getEosTokenAddress(nftAddress, klaytn.selectedAddress)).data;
-        
+        const { items: nfts } = (
+          await getEosTokenAddress(nftAddress, klaytn.selectedAddress)
+        ).data;
+
         let isStaked = false;
         let isOwned = false;
 
-        if(nfts.length !== 0){
-          for(let i = 0; i < nfts.length; i++){
+        if (nfts.length !== 0) {
+          for (let i = 0; i < nfts.length; i++) {
             // console.log(nfts[i].owner, nftAddress, nfts[i].tokenId);
-            if(nfts[i].owner === klaytn.selectedAddress){
+            if (nfts[i].owner === klaytn.selectedAddress) {
               isOwned = true;
             }
-            
+
             // const nftLendingStatus = await lendingContract.methods.stakedNft(nfts[i].owner, nftAddress, nfts[i].tokenId).call();
 
-            if(isStaked && isOwned) break;            
+            if (isStaked && isOwned) break;
           }
         }
         return { ...nftData, address: nftAddress, isStaked, isOwned };
@@ -134,7 +139,10 @@ function App() {
           <Header account={account} setAccount={setAccount} />
           <St.ContentView>
             <Routes>
-              <Route path="/" element={<Borrow whiteListNFTList={whiteListNFTList} />} />
+              <Route
+                path="/"
+                element={<Borrow whiteListNFTList={whiteListNFTList} />}
+              />
               <Route
                 path="borrow/:nftname"
                 element={<Common whiteListNFTList={whiteListNFTList} />}
